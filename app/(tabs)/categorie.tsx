21cadb8d09964359eda1a33bfc0bf18a4ac50088
Enrico Negretto <negrettoenrico@gmail.com>
2024-12-19 00:04:35 +0100

    Maledette ste Card

--- app/(tabs)/categorie.tsx
+++ app/(tabs)/categorie.tsx
@@ -5,19 +5,35 @@ import Card from "@/components/Card";
 export default function categorieScreen(){
   return (
     <View style={styles.container}>
-      <Grid container={true} spacing={4}>
-        <Grid size={4}>
-          <Text style={styles.text}>size=8</Text>
-        </Grid>
-        <Grid size={4}>
-          <Text style={styles.text}>size=4</Text>
-        </Grid>
-        <Grid size={4}>
-          <Text style={styles.text}>size=4</Text>
+      <Grid container={true} spacing={4} style={{alignItems: 'center'}}>
+        <Grid>
+          <Card
+            imgLink={'https://clicknbuy.altervista.org/wp-content/uploads/2024/12/output-onlinepngtools-1.png'}
+            title={'1'} description={'aaa'} buttonName={'A'} buttonAction={undefined}>
+          </Card>
         </Grid>
         <Grid>
-          <Card>
-
+          <Card
+            imgLink={'https://clicknbuy.altervista.org/wp-content/uploads/2024/12/output-onlinepngtools-1.png'}
+            title={'2'} description={'bbb'} buttonName={'B'} buttonAction={undefined}>
+          </Card>
+        </Grid>
+        <Grid>
+          <Card
+            imgLink={'https://clicknbuy.altervista.org/wp-content/uploads/2024/12/output-onlinepngtools-1.png'}
+            title={'3'} description={'ccc'} buttonName={'C'} buttonAction={undefined}>
+          </Card>
+        </Grid>
+        <Grid>
+          <Card
+            imgLink={'https://clicknbuy.altervista.org/wp-content/uploads/2024/12/output-onlinepngtools-1.png'}
+            title={'4'} description={'ddd'} buttonName={'D'} buttonAction={undefined}>
+          </Card>
+        </Grid>
+        <Grid>
+          <Card
+            imgLink={'https://clicknbuy.altervista.org/wp-content/uploads/2024/12/output-onlinepngtools-1.png'}
+            title={'5'} description={'fff'} buttonName={'F'} buttonAction={undefined}>
           </Card>
         </Grid>
       </Grid>
--- components/Card.tsx
+++ components/Card.tsx
@@ -4,32 +4,45 @@ import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
 import CardActions from "@mui/material/CardActions";
 import Button from "@mui/material/Button";
+import {StyleSheet} from "react-native";
+import {createTheme} from "@mui/system";
 
-export default function card(){
+export default function card({imgLink, title, description, buttonName, buttonAction}){
   return (
     <Card sx={{ maxWidth: 345 }}>
       <CardActionArea>
         <CardMedia
           component="img"
           height="140"
-          image="https://www.skyechooks.com/wp-content/uploads/2024/05/How-to-care-for-a-long-tailed-lizard-1.png"
-          alt="green iguana"
+          image={imgLink}
+          alt="image"
         />
         <CardContent>
           <Typography gutterBottom variant="h5" component="div">
-            Lizard
+            {title}
           </Typography>
           <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
+            {description}
           </Typography>
         </CardContent>
       </CardActionArea>
       <CardActions>
-        <Button size="small" color="primary">
-          Share
+        <Button size="small" color="primary" onClick={() => {buttonAction}}>
+          {buttonName}
         </Button>
       </CardActions>
     </Card>
   );
 }
+
+const styles = StyleSheet.create({
+
+});
+
+const theme = createTheme({
+  palette: {
+    background: {
+      paper: '#EFF7FF',
+    },
+  },
+});
